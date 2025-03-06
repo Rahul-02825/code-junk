8ace82de0029d0ec8d375ffbf0a88ca1182e2564
Rahul-02825 <rahul2825.kannan@gmail.com>
2025-03-06 06:51:05 +0530

    added files to path

--- apiDemo/src/path.jsx
+++ apiDemo/src/path.jsx
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Home from "./App";
+import Get from "./Get"
+import PostArray from "./PostArray"
+const queryClient = new QueryClient(); // Create a single QueryClient instance
 
 const Path = () => {
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
-    </Router>
+    return(
+     
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/Get" element={<Get />}></Route>
+                    <Route path="/PostArray" element={<PostArray />}></Route>
+                </Routes>
+            </Router>
+    )
 };
+export default Path;
 
-export default Path
+
